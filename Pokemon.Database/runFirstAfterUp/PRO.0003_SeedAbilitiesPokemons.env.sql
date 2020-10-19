0674bda2bbf3e89bc78201198c5b095883f0b936
Manuel Fernandez-Montes <devilmanu1986@outlook.es>
2020-10-19 20:53:43 +0200

    ok

--- Pokemon.Database/runFirstAfterUp/PRO.0003_SeedAbilitiesPokemons.env.sql
+++ Pokemon.Database/runFirstAfterUp/PRO.0003_SeedAbilitiesPokemons.env.sql
@@ -5,3 +5,5 @@ insert into Abilities (Id, Name) values ('dd5ffc04-1390-4d50-8a6a-e5f19ba1f495',
 insert into Pokemons (Id, Name, Age, CreatedAt) values ('fb5666f7-40a5-4aab-b54d-dd9ba6c46a8f', 'Zosterops pallidus', 61, '2020-03-19T22:56:10Z');
 insert into Pokemons (Id, Name, Age, CreatedAt) values ('f55d3651-3166-4d06-920f-e1d0e17c97a3', 'Bubulcus ibis', 12, '2020-04-20T04:52:06Z');
 insert into Pokemons (Id, Name, Age, CreatedAt) values ('bb3166fe-2b2f-437c-bc4f-8ebd83f9283c', 'Gymnorhina tibicen', 52, '2020-02-17T03:25:05Z');
+
+insert into Pokemons (Id, Name, Age, CreatedAt) values ('bb3166fe-2b2f-437c-bc4f-8ebd83f9283b', 'Troll', 99, '2020-02-17T03:25:05Z');
