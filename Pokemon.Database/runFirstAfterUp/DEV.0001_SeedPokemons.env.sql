be1fcd471c00a8ab6fc7fade701536c6265e3d85
Manuel Fernandez-Montes <devilmanu1986@outlook.es>
2020-10-19 21:43:54 +0200

    ok

--- Pokemon.Database/runFirstAfterUp/DEV.0001_SeedPokemons.env.sql
+++ Pokemon.Database/runFirstAfterUp/DEV.0001_SeedPokemons.env.sql
@@ -1,2 +1,2 @@
 insert into Pokemons (Id, Name, Age, CreatedAt) values ('f55d3651-3166-4d06-920f-e1d0e17c97a3', 'Bubulcus ibis', 12, '2020-04-20T04:52:06Z');
-insert into Pokemons (Id, Name, Age, CreatedAt) values ('f55d3651-3166-4d06-920f-e1d0e17c97a3', 'Bubulcus ibis', 12, '2020-04-20T04:52:06Z');
+insert into Pokemons (Id, Name, Age, CreatedAt) values ('f55d3651-3166-4d06-920f-e1d0e17c97a4', 'Bubulcus', 12, '2020-04-20T04:52:06Z');
