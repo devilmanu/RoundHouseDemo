f4bdddfe29be5225ec55f9e463d6843da8a3b27a
Manuel Fernandez-Montes <devilmanu1986@outlook.es>
2020-10-19 21:01:05 +0200

    no me dejan serr malvado

--- Pokemon.Database/up/0001_InitialSchema.sql
+++ Pokemon.Database/up/0001_InitialSchema.sql
@@ -1,8 +1,8 @@
-﻿-- CREATE TABLE [Pokemons] (
---     [Id] uniqueidentifier NOT NULL,
---     [Name] nvarchar(max) NOT NULL,
---     [CreatedAt] datetimeoffset NOT NULL,
---     [Age] int NOT NULL,
---     CONSTRAINT [PK_Pokemons] PRIMARY KEY ([Id])
--- );
--- GO
+﻿CREATE TABLE [Pokemons] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    [CreatedAt] datetimeoffset NOT NULL,
+    [Age] int NOT NULL,
+    CONSTRAINT [PK_Pokemons] PRIMARY KEY ([Id])
+);
+GO
